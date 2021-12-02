3988d65707870c4dc780f1d7e5705f6b6cdd6fa8
Artyom <artem.a.dyachenko@gmail.com>
2021-12-02 09:25:38 +0300

    update project style

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import collections
 import datetime
 import pandas
+import sys
+import argparse
 
 from http.server import HTTPServer
 from http.server import SimpleHTTPRequestHandler
@@ -8,35 +10,50 @@ from jinja2 import Environment
 from jinja2 import FileSystemLoader
 from jinja2 import select_autoescape
 
-excel_wine = pandas.read_excel('wine.xlsx',na_values=['nan', 'None'], keep_default_na=False)
-dict_wine = excel_wine.to_dict(orient='records')
-production_dict = collections.defaultdict(list)
 
-for wine in dict_wine:
-    production_dict[wine['Категория']].append(wine)
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n', help="Путь к файлу", default='wine.xlsx')
 
-
-event1 = datetime.datetime(year=1920, month=12, day=12, hour=1)
-formatted_date_1 = event1.strftime('%Y')
-event2 = datetime.datetime.today()
-formatted_date_2 = event2.strftime('%Y')
-year_time = int(formatted_date_2)-int(formatted_date_1)
+    return parser
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+if __name__ == '__main__':
+    parser = createParser()
+    namespace = parser.parse_args()
 
-template = env.get_template('template.html')
+    print(namespace.n)
 
-rendered_page = template.render(
-    time_found=f'Уже {year_time} год с вами',
-    production_dict=production_dict
-)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    wines = pandas.read_excel(f'{namespace.n}',na_values=['nan', 'None'], keep_default_na=False).to_dict(orient='records')
+    production = collections.defaultdict(list)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    for wine in wines:
+        production[wine['Категория']].append(wine)
+
+
+    year_foundation = datetime.datetime(year=1920, month=12, day=12, hour=1)
+    winery_age= datetime.datetime.today().year - year_foundation.year
+
+    for price in production:
+        print(production)
+        print(price)
+        end
+
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(
+        time_found=f'Уже {winery_age} год с вами',
+        production=production
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
